2b3a8b06fd062aee68d1d57f205a1c761b6d5f84
pkp5102 <prerakgit510@gmail.com>
2024-02-05 10:33:16 +0530

    initial

--- dj3/dj3/urls.py
+++ dj3/dj3/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from school import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.TemplateView.as_view(template_name='school/home.html'),name='home'),
+    # path('',views.TemplateView.as_view(template_name='school/home.html'),name='home'),
+    path('',views.HomeTemplateView.as_view(),name='home'),
       
 ]
