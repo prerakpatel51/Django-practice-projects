97a0c54de3c065544a644d2418d7a09102b5a381
pkp5102 <prerakgit510@gmail.com>
2024-02-05 11:01:49 +0530

    initial

--- dj3/dj3/urls.py
+++ dj3/dj3/urls.py
@@ -20,6 +20,7 @@ from school import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('',views.TemplateView.as_view(template_name='school/home.html'),name='home'),
-    path('',views.HomeTemplateView.as_view(),name='home'),
+    # path('',views.HomeTemplateView.as_view(),name='home'),
+    path('',views.HomeTemplateView.as_view(extra_context={'course':'Python'}),name='home'),
       
 ]
--- dj3/school/views.py
+++ dj3/school/views.py
@@ -3,3 +3,10 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class HomeTemplateView(TemplateView):
     template_name='school/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = 'prerak'
+        context["rollno"]=65
+        return context
+    
